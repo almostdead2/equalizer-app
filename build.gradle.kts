45222a8df25105a1f5b64dae16490cb785f9813d
almostdead2 <128363011+almostdead2@users.noreply.github.com>
2025-08-07 10:33:55 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-    id("com.android.application") version "8.1.1" apply false
-    kotlin("android") version "1.9.10" apply false
+    id("com.android.application") version "8.4.0" apply false
+    kotlin("android") version "1.9.23" apply false
 }
